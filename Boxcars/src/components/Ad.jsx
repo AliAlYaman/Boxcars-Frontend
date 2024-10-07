593dbf8298e48f2e384bd4304914d2c2a3ae31de
Ali Al Yaman <alialyaman750@gmail.com>
2024-10-07 12:44:19 +0300

    Deployment push

--- Boxcars/src/components/Ad.jsx
+++ Boxcars/src/components/Ad.jsx
@@ -1,6 +1,6 @@
 export const Ad = () => {
     return (
-        <div className="bg-[url('assets/images/background.png')] bg-cover bg-center h-[500px] w-full flex flex-col items-start justify-center gap-4 text-white px-8 md:px-20 lg:px-40">
+        <div className="bg-[url('https://www.bmw.ua/content/dam/bmw/marketB4R1/bmw_ua/donnected-drive/connectedrive_support_xxl.jpg/jcr:content/renditions/cq5dam.resized.img.1680.large.time1710400387813.jpg')] bg-cover bg-center h-[500px] w-full flex flex-col items-start justify-center gap-4 text-white px-8 md:px-20 lg:px-40">
             <h1 className="text-3xl md:text-4xl lg:text-5xl font-dmSans font-medium mb-4 text-start">
                 We Make Finding The<br/>
                 Right Car Simple
